e66e33bfa845958ab84a7a9fe2e5bf4dbc332023
Petro Knysh <peteknysh@gmail.com>
2018-07-18 01:28:35 +0300

    added logging

--- src/common/common.go
+++ src/common/common.go
@@ -1,7 +1,22 @@
 package common
 
+import (
+	"os"
+	"log"
+)
+
 func Check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func InitLogging()  {
+	logFile, err := os.OpenFile("localization_log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
 }
