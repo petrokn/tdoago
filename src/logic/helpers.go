e89d601c19aa8a725a93ac1ba69517e6b1e5edec
Petro Knysh <peteknysh@gmail.com>
2018-07-26 01:00:47 +0300

    attempt to transfer dummiest func

--- src/logic/helpers.go
+++ src/logic/helpers.go
@@ -1,1 +1,13 @@
 package logic
+
+func perDelta(start int, end int, delta int) (int, int) {
+	current := start
+
+	for current < end && current+delta < end {
+		//yield (curr, curr + delta)
+		current += delta
+		//yield (curr, end)
+	}
+
+	return 0, 0
+}
