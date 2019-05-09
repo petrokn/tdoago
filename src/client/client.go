bc22d6446111738e737c382e46d05a385baa1048
Petro Knysh <peteknysh@gmail.com>
2019-05-09 23:51:41 +0300

    some abstraction regrading mics

--- src/client/client.go
+++ src/client/client.go
@@ -1,1 +1,24 @@
 package client
+
+import "tdoago/src/modeling"
+
+type Client interface {
+	Send(microphones []*modeling.Microphone) []error
+	SendLive(microphones []*modeling.Microphone) []error
+}
+
+type BaseClient struct{}
+
+func (bc *BaseClient) Send(microphones []*modeling.Microphone) []error {
+	errors := make([]error, len(microphones))
+
+	for _, microphone := range microphones {
+		errors = append(errors, (*microphone).(modeling.Microphone).SendData())
+	}
+
+	return errors
+}
+
+func (bc *BaseClient) SendLive(microphones []*modeling.Microphone) []error {
+	return nil
+}
--- src/modeling/microphones.go
+++ src/modeling/microphones.go
@@ -1,5 +1,15 @@
 package modeling
 
+type Microphone interface {
+	SendData() error
+}
+
+type MicrophoneProxy struct{}
+
+func (mp *MicrophoneProxy) SendData() error {
+	return nil
+}
+
 func GenerateDataForMicrophones() {
 
 }
